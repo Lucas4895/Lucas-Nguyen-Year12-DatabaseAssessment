e7daa2c65b3ba10410175dcf9a5132259b6e000b
Lucas4895 <23554ln@hvhs.school.nz>
2026-06-04 10:49:45 +1200

    2026.6.4 -- Test Commit

---
+++ fb_setup.js
@@ -0,0 +1,28 @@
+/**************************************************************/
+// fb_initialise()
+// Initialize firebase, connect to the Firebase project.
+// 
+// Find the config data in the Firebase console. Cog wheel > Project Settings > General > Your Apps > SDK setup and configuration > Config
+//
+// Input:  n/a
+// Return: n/a
+/**************************************************************/
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDtQWLIdVm1wXxx3_nLL5jmDDjl_MH-cPY",
+  authDomain: "comp-2026-lucas-nguyen.firebaseapp.com",
+  databaseURL: "https://comp-2026-lucas-nguyen-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "comp-2026-lucas-nguyen",
+  storageBucket: "comp-2026-lucas-nguyen.firebasestorage.app",
+  messagingSenderId: "691283081178",
+  appId: "1:691283081178:web:61c724a6e33d905fd7f2e3",
+  measurementId: "G-JKFYTZL473"
+};
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // This log prints the firebase object to the console to show that it is working.
+  // As soon as you have the script working, delete this log.
+  console.log("Firebase initialize finished:");
+  console.log(firebase);
