1709a1cbdc226d17fdbbf6f98056f635af72b3c8
Lucas4895 <23554ln@hvhs.school.nz>
2026-06-30 02:43:03 +0000

    2026.6.30 -- Leaderboard (p2) ----------------------------- - Added leaderboard for GeoDash - Leaderboards now automatically refresh on page load

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/****************************************************************************/
+//write functions
 function fb_write(){
     // Check if the GLOBAL_user variable actually has data yet
     if (!GLOBAL_user) {
@@ -17,10 +19,6 @@ function fb_write(){
     console.log("Data Saved")
 }
 
-
-/****************************************************************************/
-//write functions
-/***************************************************************************/
 function fb_writeScore() {  //catCarrot's write function
     if (!GLOBAL_user) {
         console.log("No logged-in user found. Score not saved to database.");
@@ -55,9 +53,9 @@ function fb_writeScore2() { //GeoDash's write function
     console.log("Score of " + score + " saved to Firebase for user " + uid);
 }
 /****************************************************************************/
-//write functions end
-/***************************************************************************/
 
+/****************************************************************************/
+//Leaderboard -- CatCarrot
 function fb_readLeaderboard() {
     console.log("Fetching leaderboard...");
     
@@ -91,3 +89,45 @@ function displayLeaderboard(snapshot) {
         listElement.appendChild(listItem);
     });
 }
+/****************************************************************************/
+
+/***************************************************************************/
+//Leaderboard -- Geo Dash
+function fb_readLeaderboard2() {
+    console.log("Fetching leaderboard...");
+    
+    firebase.database()
+        .ref("/geoDash")
+        .orderByChild("score")
+        .once("value", displayLeaderboard2);
+}
+
+function displayLeaderboard2(snapshot) {
+    const listElement = document.getElementById("leaderboard-list2");
+    
+    // Clear out any old scores inside the list so we don't accidentally duplicate them
+    listElement.innerHTML = "";
+    let scoreboardArray2 = [];
+
+    snapshot.forEach(function(child) {
+        const data = child.val();
+        scoreboardArray2.push({
+            username: data.username,
+            score: data.score
+        });
+    });
+
+    scoreboardArray2.reverse();
+
+    // Now loop through sorted array and create HTML list items <li>
+    scoreboardArray2.forEach(function(player) {
+        const listItem = document.createElement("li");
+        listItem.textContent = player.username + " — " + player.score + " points";
+        listElement.appendChild(listItem);
+    });
+}
+/****************************************************************************/
+
+//Refresh leaderboards as soon as page's loaded
+fb_readLeaderboard()
+fb_readLeaderboard2()
